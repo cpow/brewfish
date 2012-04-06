93c7678090927d00bfd648e2961d0d58ad344cd3
Chris Power <cpow85@gmail.com>
2012-04-06 18:27:04 -0400

    initial commit for the brewfish website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewfish::Application.config.secret_token = '9a2c959ca00b413b91ea2c60ec9aee9d7b3afb0a9fb9fa62a9c9d4b7c15ef1fd571c1f81bcf8bfc1722dfa203c2dadc5253106e0f98c2247f5bc1dadf7bdc62f'
