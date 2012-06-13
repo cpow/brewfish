6e073de7caa2546dec100c90a54a7b068cff85c3
Cpow <cpow85@gmail.com>
2012-06-13 19:42:34 -0400

    removed some white spaces I suppose

--- vendor/extensions/players/app/models/refinery/players/player.rb
+++ vendor/extensions/players/app/models/refinery/players/player.rb
@@ -4,11 +4,9 @@ module Refinery
       self.table_name = 'refinery_players'      
       has_many :stats, :class_name => '::Refinery::Stats::Stat' 
       attr_accessible :stat_id, :bio, :photo_id, :position, :name
-    
       acts_as_indexed :fields => [:bio, :name]
 
       validates :bio, :presence => true, :uniqueness => true
-          
       belongs_to :photo, :class_name => '::Refinery::Image'
 
       def sum_stats(symbol)
